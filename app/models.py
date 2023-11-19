d68a0078b8d72cdcd29b923d3bf254691dc856d8
Anup Dahal <adahal39@gmail.com>
2023-11-19 14:37:43 +0545

    Feat: Mills data uploaded

--- app/models.py
+++ app/models.py
@@ -12,6 +12,8 @@ class Facility(models.Model):
     id = models.AutoField(primary_key=True)
     facilities_eq_id = models.CharField(max_length=255, help_text=_(
         "facilities_eq_id"), verbose_name=_("facilities_eq_id"), null=True)
+    facilities_name = models.CharField(max_length=255, help_text=_(
+        "facilities_name"), verbose_name=_("facilities_name"), null=True)
     facilities_address = models.CharField(max_length=500, help_text=_(
         "facilities_address"), verbose_name=_("facilities_address"), null=True)
     facilities_type = models.CharField(max_length=255, help_text=_(
@@ -45,3 +47,112 @@ class Facility(models.Model):
 
     def __str__(self):
         return str(self.facilities_address)
+
+
+class Refinery(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    refinery_eq_id = models.CharField(max_length=255, help_text=_(
+        "refinery_eq_id"), verbose_name=_("refinery_eq_id"), null=True)
+    refinery_name = models.CharField(max_length=255, help_text=_(
+        "refinery_name"), verbose_name=_("refinery_name"), null=True)
+    refinery_address = models.CharField(max_length=500, help_text=_(
+        "refinery_address"), verbose_name=_("refinery_address"), null=True)
+    refinery_type = models.CharField(max_length=255, help_text=_(
+        "refinery_type"), verbose_name=_("refinery_type"), null=True)
+    refinery_lat = models.DecimalField(
+        max_digits=9, decimal_places=6,
+        help_text="refinery_lat", verbose_name="refinery_lat",
+        null=True
+    )
+    refinery_long = models.DecimalField(
+        max_digits=9, decimal_places=6,
+        help_text="refinery_long", verbose_name="refinery_long",
+        null=True
+    )
+    refinery_rspo = models.CharField(max_length=255, help_text=_(
+        "refinery_rspo"), verbose_name=_("refinery_rspo"), null=True)
+    refinery_date_update = models.CharField(max_length=255, help_text=_(
+        "refinery_date_update"), verbose_name=_("refinery_date_update"), null=True)
+    created_at = models.DateTimeField(default=timezone.now, help_text=_(
+        "Creation date"), verbose_name=_("Created at"))
+    geom = models.PointField(srid=4326, dim=2, null=True)
+    is_display = models.BooleanField(default=True)
+    is_edited = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = GeoManager()
+
+    class Meta:
+        verbose_name = _("Refinery")
+        verbose_name_plural = _("Refineries")
+
+    def __str__(self):
+        return str(self.refinery_address)
+
+
+class Mill(models.Model):
+    id = models.AutoField(primary_key=True)
+    mill_company_name = models.CharField(max_length=255, help_text=_(
+        "mill_company_name"), verbose_name=_("mill_company_name"), null=True)
+    mill_company_group_id = models.CharField(max_length=255, help_text=_(
+        "mill_company_group_id"), verbose_name=_("mill_company_group_id"), null=True)
+    mill_company_group = models.CharField(max_length=255, help_text=_(
+        "mill_company_group"), verbose_name=_("mill_company_group"), null=True)
+    mill_country = models.CharField(max_length=255, help_text=_(
+        "mill_country"), verbose_name=_("mill_country"), null=True)
+    mill_province = models.CharField(max_length=255, help_text=_(
+        "mill_province"), verbose_name=_("mill_province"), null=True)
+    mill_district = models.CharField(max_length=255, help_text=_(
+        "mill_district"), verbose_name=_("mill_district"), null=True)
+    mill_address = models.CharField(max_length=500, help_text=_(
+        "mill_address"), verbose_name=_("mill_address"), null=True)
+    mill_type = models.CharField(max_length=255, help_text=_(
+        "mill_type"), verbose_name=_("mill_type"), null=True)
+    mill_lat = models.DecimalField(
+        max_digits=9, decimal_places=6,
+        help_text="mill_lat", verbose_name="mill_lat",
+        null=True
+    )
+    mill_long = models.DecimalField(
+        max_digits=9, decimal_places=6,
+        help_text="mill_long", verbose_name="mill_long",
+        null=True
+    )
+    mill_rspo = models.CharField(max_length=255, help_text=_(
+        "mill_rspo"), verbose_name=_("mill_rspo"), null=True)
+    mill_mspo = models.CharField(max_length=255, help_text=_(
+        "mill_mspo"), verbose_name=_("mill_mspo"), null=True)
+    mill_capacity = models.CharField(max_length=255, help_text=_(
+        "mill_capacity"), verbose_name=_("mill_capacity"), null=True)
+    mill_methane_capture = models.CharField(max_length=255, help_text=_(
+        "mill_methane_capture"), verbose_name=_("mill_methane_capture"), null=True)
+    mill_deforestation_risk = models.CharField(max_length=255, help_text=_(
+        "mill_deforestation_risk"), verbose_name=_("mill_deforestation_risk"), null=True)
+    mill_legal_prf_risk = models.CharField(max_length=255, help_text=_(
+        "mill_legal_prf_risk"), verbose_name=_("mill_legal_prf_risk"), null=True)
+    mill_legal_production_forest = models.CharField(max_length=255, help_text=_(
+        "mill_legal_production_forest"), verbose_name=_("mill_legal_production_forest"), null=True)
+    mill_legal_conservation_area = models.CharField(max_length=255, help_text=_(
+        "mill_legal_conservation_area"), verbose_name=_("mill_legal_conservation_area"), null=True)
+    mill_legal_landuse_risk = models.CharField(max_length=255, help_text=_(
+        "mill_legal_landuse_risk"), verbose_name=_("mill_legal_landuse_risk"), null=True)
+    mill_complex_supplybase_risk = models.CharField(max_length=255, help_text=_(
+        "mill_complex_supplybase_risk"), verbose_name=_("mill_complex_supplybase_risk"), null=True)
+    mill_date_update = models.CharField(max_length=255, help_text=_(
+        "mill_date_update"), verbose_name=_("mill_date_update"), null=True)
+    created_at = models.DateTimeField(default=timezone.now, help_text=_(
+        "Creation date"), verbose_name=_("Created at"))
+    geom = models.PointField(srid=4326, dim=2, null=True)
+    is_display = models.BooleanField(default=True)
+    is_edited = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = GeoManager()
+
+    class Meta:
+        verbose_name = _("Mill")
+        verbose_name_plural = _("Mills")
+
+    def __str__(self):
+        return str(self.mill_address)
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import viewsets
-from .models import Facility
+from .models import Facility, Refinery, Mill
 from .tasks import handleExampleTask
 from .serializer import FileUploadSerializer, FacilitySerializer
 from django.contrib.gis.geos import Point
@@ -33,33 +33,84 @@ class FileUploadAPIView(generics.CreateAPIView):
         sheet = serializer.validated_data['sheet']
         excel_file = serializer.validated_data['file']
 
-        print(excel_file, 'excel file')
-        print(sheet, 'sheet')
-
-        # Process Excel file using Pandas
         try:
-            print(excel_file, 'file excel')
-            df = pd.read_excel(excel_file, sheet_name="Facilites")
-            print(df.head(), 'df head')
+            df = pd.read_excel(excel_file, sheet_name=sheet)
             df.fillna(0, inplace=True)
-            df['geom'] = df.apply(lambda row: Point(
-                row['facilities_long'], row['facilities_lat']), axis=1)
-            facilities_data = df.to_dict(orient='records')
+            if sheet == "Facilites":
+                df['geom'] = df.apply(lambda row: Point(
+                    row['facilities_long'], row['facilities_lat']), axis=1)
+                facilities_data = df.to_dict(orient='records')
 
-            # # # Create Facility objects
-            # Facility.objects.bulk_create([Facility(
-            #     facilities_eq_id=data['facilities_eq_id'],
-            #     facilities_address=data['facilities_address'],
-            #     facilities_type=data['facilities_type'],
-            #     facilities_lat=data['facilities_lat'],
-            #     facilities_long=data['facilities_long'],
-            #     facilites_rspo=data['facilites_rspo'],
-            #     facilites_date_update=data['facilites_date_update'],
-            #     geom=data['geom'],
-            #     # Add other fields accordingly
-            # ) for data in facilities_data])
+                # # Create Facility objects
+                Facility.objects.bulk_create([Facility(
+                    facilities_eq_id=data['facilities_eq_id'],
+                    facilities_name=data['facilities_name'],
+                    facilities_address=data['facilities_address'],
+                    facilities_type=data['facilities_type'],
+                    facilities_lat=data['facilities_lat'],
+                    facilities_long=data['facilities_long'],
+                    facilites_rspo=data['facilites_rspo'],
+                    facilites_date_update=data['facilites_date_update'],
+                    geom=data['geom'],
+                    # Add other fields accordingly
+                ) for data in facilities_data])
 
-            return Response({'message': 'Data uploaded successfully'}, status=status.HTTP_201_CREATED)
+                return Response({'message': f'{sheet} Data uploaded successfully'}, status=status.HTTP_201_CREATED)
+            if sheet == "Refinery":
+                df['geom'] = df.apply(lambda row: Point(
+                    row['refinery_long'], row['refinery_lat']), axis=1)
+                refinery_data = df.to_dict(orient='records')
+
+                # # Create Refinery objects
+                Refinery.objects.bulk_create([Refinery(
+                    refinery_eq_id=data['refinery_eq_id'],
+                    refinery_name=data['refinery_name'],
+                    refinery_address=data['refinery_address'],
+                    refinery_type=data['refinery_type'],
+                    refinery_lat=data['refinery_lat'],
+                    refinery_long=data['refinery_long'],
+                    refinery_rspo=data['refinery_rspo'],
+                    refinery_date_update=data['refinery_date_update'],
+                    geom=data['geom'],
+                    # Add other fields accordingly
+                ) for data in refinery_data])
+
+                return Response({'message': f'{sheet} Data uploaded successfully'}, status=status.HTTP_201_CREATED)
+
+            if sheet == "Mill":
+                df['geom'] = df.apply(lambda row: Point(
+                    row['mill_long'], row['mill_lat']), axis=1)
+                mill_data = df.to_dict(orient='records')
+                print(df.head().to_dict(orient='records'))
+
+                Mill.objects.bulk_create([Mill(
+                    mill_company_name=data['mill_company_name'],
+                    mill_company_group_id=data['mill_company_group_id'],
+                    mill_company_group=data['mill_company_group'],
+                    mill_country=data['mill_country'],
+                    mill_province=data['mill_province'],
+                    mill_district=data['mill_district'],
+                    mill_address=data['mill_address'],
+                    mill_type=data['mill_type'],
+                    mill_lat=data['mill_lat'],
+                    mill_long=data['mill_long'],
+                    mill_rspo=data['mill_rspo'],
+                    mill_mspo=data['mill_mspo'],
+                    mill_capacity=data['mill_capacity'],
+                    mill_methane_capture=data['mill_methane_capture'],
+                    mill_deforestation_risk=data['mill_deforestation_risk'],
+                    mill_legal_prf_risk=data['mill_legal_prf_risk'],
+                    mill_legal_production_forest=data['mill_legal_production_forest'],
+                    mill_legal_conservation_area=data['mill_legal_conservation_area'],
+                    mill_legal_landuse_risk=data['mill_legal_landuse_risk'],
+                    mill_complex_supplybase_risk=data['mill_complex_supplybase_risk'],
+                    mill_date_update=data['mill_date_update'],
+                    geom=data['geom'],
+                    # Add other fields accordingly
+                ) for data in mill_data])
+
+                return Response({'message': f'{sheet} Data uploaded successfully'}, status=status.HTTP_201_CREATED)
+
         except Exception as e:
             return Response({'error': f'Error processing file: {str(e)}'}, status=status.HTTP_400_BAD_REQUEST)
 
